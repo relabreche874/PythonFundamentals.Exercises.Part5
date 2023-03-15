9bd4ada42991b2db1272150ef7ebb523cb66e7b6
Robert <robert@Zipcoders-MacBook-Pro-60.local>
2023-03-15 15:20:12 -0400

    Updated stringutils

--- string_utils.py
+++ string_utils.py
@@ -3,28 +3,29 @@ def str_len(str_in: str) -> str:
     """
     Given a string parameter, this function should return the length of the parameter.
     """
-    pass  # remove pass statement and implement me
+    return len(str_in)
+# remove pass statement and implement me
 
 
 def first_char(str_in: str) -> str:
     """
     Given a string parameter, this function should return the first letter of the parameter.
     """
-    pass  # remove pass statement and implement me
+    return str_in[0]
 
 
 def last_char(str_in: str) -> str:
     """
     Given a string parameter, this function should return the last letter of the parameter..
     """
-    pass  # remove pass statement and implement me
+    return str_in[-1]
 
 
 def input_has_substring(str_in: str, sub_str_in: str) -> bool:
     """
     This function determines if the substring exists within the string. Returns True or False.
     """
-    pass  # remove pass statement and implement me
+    return sub_str_in in str_in
 
 
 def substring(str_in: str, start: int, stop: int) -> str:
@@ -36,7 +37,7 @@ def substring(str_in: str, start: int, stop: int) -> str:
     start -- starting position of the input parameter to start the substring (inclusive)
     stop -- stopping position of the input parameter to stop the substring (exclusive)
     """
-    pass  # remove pass statement and implement me
+    return str_in[start:stop]
 
 
 def opposite_case(str_in: str) -> str:
@@ -45,4 +46,4 @@ def opposite_case(str_in: str) -> str:
     Example: 
     When input = "Python" the function returns "pYTHON"
     """
-    pass  # remove pass statement and implement me
+    return str_in.swapcase()
